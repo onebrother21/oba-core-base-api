961847666e916c9884c1ed34c7559924e559ed61
onebrother <service.onebrother@gmail.com>
2021-12-11 16:51:27 -0600

    rebuilding with minor changes

--- test/tests/index.ts
+++ test/tests/index.ts
@@ -1,15 +1,16 @@
 import {J} from "../utils";
 import {obaCoreBaseComponentsTests} from "./components";
+import OB from "@onebro/oba-common";
 
 export const finalCheckComponents = () => J.desc("FINAL CHECK",() => {
   it("Init",async () => {
     const {base} = await J.initApp("OBA_CORE_BASE");
-    console.log(base);
+    OB.log(base);
   },1E9);
 });
 export const wrapup = () => J.desc("WRAPUP",() => {
   it("Wrapup",async () => {
-    console.log("complete");
+    OB.log("complete");
   },1E9);
 });
 export const allTests = () => J.desc("ALL TESTS",() => {
